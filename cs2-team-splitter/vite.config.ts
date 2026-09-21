b1940fd9bc9cd8a6fe3c075a653a5ae645fe47be
Mohammad Yusuf <mohammad.yusuf@wfp.org>
2026-09-21 14:03:11 +0300

    change base

--- cs2-team-splitter/vite.config.ts
+++ cs2-team-splitter/vite.config.ts
@@ -4,5 +4,5 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/cs2-team-splitter/",
+  base: "/cs2Splitter/cs2-team-splitter/",
 });
